b86fc6b56dc77daa1ade37dfcfc51a5bd7f1b541
yutoyama <tymy1230@gmail.com>
2020-06-22 18:59:47 +0900

    migrationファイルがかけていた

--- db/migrate/20200505105013_create_orders.rb
+++ db/migrate/20200505105013_create_orders.rb
@@ -1,7 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-
+      t.integer :end_user_id
+      t.integer :total_price
+      t.string :address_user_name
+      t.string :address
+      t.string :postal_code
+      t.integer :delivery_price
+      t.integer :pay_way, default: 0
+      t.integer :order_status, default: 0
       t.timestamps
     end
   end
