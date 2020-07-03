0a3d895020e841b533b3324a25f834267e857e22
yutoyama <tymy1230@gmail.com>
2020-07-03 23:42:16 +0900

    キタコレ終わった

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,12 +2,13 @@ class Admin::OrderDetailsController < ApplicationController
 
     def update
         detail = OrderDetail.find(params[:id])
+        count = OrderDetail.where(order_id: detail.order.id).count
         detail.update(detail_params)
 
         if detail.production_status_before_type_cast == 2
             detail.order.order_status = 2
             detail.order.update(order_params)
-        elsif detail.production_status_before_type_cast == 3
+        elsif detail.production_status_before_type_cast == 3 && detail.order.order_details.count == count
             detail.order.order_status = 3
             detail.order.update(order_params)
         end
